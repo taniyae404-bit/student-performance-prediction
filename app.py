0dffef8b0c6d579c68770154e6a92082d8750a76
Alishba Ejaz <taniyae404@gmail.com>
2026-09-03 02:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -431,9 +431,7 @@ elif page == "💬 Ask Student AI":
 
         else:
 
-            try:
-
-    client = genai.Client(
+            try: client = genai.Client(
         api_key=st.secrets["GEMINI_API_KEY"]
     )
 
